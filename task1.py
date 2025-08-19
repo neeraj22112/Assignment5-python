a2ef178c50bc6e10cb23d55f56bf73a5372be7b0
Neeraj Bhardwaj <140228074+neeraj22112@users.noreply.github.com>
2025-08-20 00:26:53 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+stud_marks={
+    'Neeraj':75,
+    'Rahul':85,
+    'Nitin':89,
+    'Bipul':79,
+    'Anurag':92,
+    'Rohit':72,
+}
+name = input("Enter the name og the student: ")
+if name in stud_marks:
+    print(f"{name}'s marks is:{stud_marks[name]}")
+else:
+    print(f"{name} named student not found")
---
+++ task2.py
@@ -0,0 +1,7 @@
+numbers=list(range(1,11))
+a = numbers[0:5]
+a.reverse()
+
+print("Original list: ", numbers)
+print("Extracted first five elements: ", numbers[0:5])
+print("Reversed extracted elements: ", a)
