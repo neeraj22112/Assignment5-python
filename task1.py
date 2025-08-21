f99e00fbc26a0af8814b2e110ebd41b78458b969
Neeraj Bhardwaj <140228074+neeraj22112@users.noreply.github.com>
2025-08-21 23:24:31 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -6,8 +6,9 @@ stud_marks={
     'Anurag':92,
     'Rohit':72,
 }
-name = input("Enter the name og the student: ")
+name = input("Enter the name of the student: ")
 if name in stud_marks:
     print(f"{name}'s marks is:{stud_marks[name]}")
 else:
+
     print(f"{name} named student not found")
